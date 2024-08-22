7dc1671455b10c4587af6f0f3bf2e94cc07d4d2d
hwelsters <hwelsters>
2024-08-22 11:16:21 -0700

    binit.

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/dust-0.5/tr
 python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/fog-0.5/train" --output_dir "outputs/fog-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
 python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/maple_leaf-0.5/train" --output_dir "outputs/maple_leaf-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
 python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/normal/train" --output_dir "outputs/normal" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/rain-0.5" --output_dir "outputs/rain-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/snow-0.5" --output_dir "outputs/snow-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
+python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/rain-0.5/train" --output_dir "outputs/rain-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
+python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/snow-0.5/train" --output_dir "outputs/snow-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
 
 # python3 src/eval.py
