8bf7756aab0d8f2d37017eae5da585efcf4e5a2b
hwelsters <hwelsters>
2024-08-21 21:20:12 -0700

    binit.

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/dust-0.5" -
 # python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/rain-0.5" --output_dir "outputs/rain-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
 # python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/snow-0.5" --output_dir "outputs/snow-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
 
-python3 src/eval.py
+# python3 src/eval.py
