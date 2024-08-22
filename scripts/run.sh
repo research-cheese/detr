8bc5c0d1f2d6a270e37a22465c4147254dc59556
hwelsters <hwelsters>
2024-08-22 11:33:00 -0700

    binit.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 source venv/bin/activate
 
-# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/train" --output_dir "outputs/train" --resume "detr-r50_no-class-head.pth"  --num_classes 4 --epochs 40
-# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/dust-0.5/train" --output_dir "outputs/dust-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/fog-0.5/train" --output_dir "outputs/fog-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/maple_leaf-0.5/train" --output_dir "outputs/maple_leaf-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/normal/train" --output_dir "outputs/normal" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/rain-0.5/train" --output_dir "outputs/rain-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
-python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/snow-0.5/train" --output_dir "outputs/snow-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 140
+# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/train" --output_dir "outputs/train" --resume "detr-r50_no-class-head.pth"  --num_classes 4 --epochs 20
+# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/dust-0.5/train" --output_dir "outputs/dust-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
+# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/fog-0.5/train" --output_dir "outputs/fog-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
+# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/maple_leaf-0.5/train" --output_dir "outputs/maple_leaf-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
+# python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/normal/train" --output_dir "outputs/normal" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
+python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/rain-0.5/train" --output_dir "outputs/rain-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
+python3 ./detr/main.py --dataset_file "custom" --coco_path "./aerial/snow-0.5/train" --output_dir "outputs/snow-0.5" --resume "outputs/train/checkpoint.pth"  --num_classes 4 --epochs 40
 
 python3 src/eval.py
